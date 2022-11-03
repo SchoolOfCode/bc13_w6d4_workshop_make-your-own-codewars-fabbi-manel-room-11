bfd49c86f2e8ea4e8f8e1c3b0c9f86230c8ca483
fabbiha <sfabbiha@gmail.com>
2022-11-03 16:46:31 +0000

    removed solution code and added description for the codewarriors

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-//Return the Coaches names that have more than two vowels
-//Taking in the coaches names
-//Calculate the vowels inside each coach name
+// We want only the coaches names that contain vowels, so make the function so that it ONLY returns those with 2 or more vowels
+// Good luck! If there are any issues with our code, please let us know! Thank you.
 
 export function coachesNamesWithVowels(name) {
-  //Write your code here
+  // Write your code here
 }
--- main.test.js
+++ main.test.js
@@ -1,13 +1,6 @@
-//Import Jest command from Jest globals Done
-//Import the function from main.js  Done
-
 import { test, expect } from "@jest/globals";
 import { coachesNamesWithVowels } from "./main.js";
 
-// Label the test as "Returning the coaches names with two or more vowels" DONE
-//  - Our test will take an array of coaches names DONE
-//  Coaches names: Arshi, Charlotte, Kazeem, Liz, Loz, Hamza DONE
-//  - Our test will expect Arshi, Charlotte, Kazeem and Hamza DONE
 
 test("Returning the coaches names with two or more vowels", () => {
   const actual = coachesNamesWithVowels([
