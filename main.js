ad1d8ce299f1d6ef73ce259259f4c56668f040b8
fabbiha <sfabbiha@gmail.com>
2022-11-03 16:40:23 +0000

    completed testing for our function

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-/* 
-👉 Write your kata here!
-*/ //Return the Coaches names that have more than two vowels
+//Return the Coaches names that have more than two vowels
 //Taking in the coaches names
 //Calculate the vowels inside each coach name
-
-//👉 Write the function your CodeWarriors will start with below here:
 
 export function coachesNamesWithVowels(name) {
   //Write your code here
